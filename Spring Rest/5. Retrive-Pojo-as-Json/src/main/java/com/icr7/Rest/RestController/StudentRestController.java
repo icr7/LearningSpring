1dd4cb34434ff5e139ab6dc0b12a36050d71d74c
ishwar chaiser <icr7.dev@outlook.com>
2021-07-01 13:21:30 +0530

    PostConstruct

--- Spring Rest/5. Retrive-Pojo-as-Json/src/main/java/com/icr7/Rest/RestController/StudentRestController.java
+++ Spring Rest/5. Retrive-Pojo-as-Json/src/main/java/com/icr7/Rest/RestController/StudentRestController.java
@@ -3,6 +3,8 @@ package com.icr7.Rest.RestController;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,15 +14,22 @@ import com.icr7.Rest.entity.Student;
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
-	
-	//define endpoints for "/students" -return list of students
-	
+	List<Student> theStudents;
+
+	// define @PopstConstruct to load the student data..
+	@PostConstruct
+	public void loadData() {
+		theStudents = new ArrayList<>();
+		theStudents.add(new Student("Ishwar", "Singh"));
+		theStudents.add(new Student("Mohit", "Chaiser"));
+		theStudents.add(new Student("Poornima", "Patel"));
+	}
+
+	// define endpoints for "/students" -return list of students
+
 	@GetMapping("/students")
-	public List<Student> getStudents(){
-		List<Student> theStudents= new ArrayList<>();
-		theStudents.add(new Student("Ishwar","Singh"));
-		theStudents.add(new Student("Mohit","Chaiser"));
-		theStudents.add(new Student("Poornima","Patel"));
+	public List<Student> getStudents() {
+
 		return theStudents;
 	}
 
